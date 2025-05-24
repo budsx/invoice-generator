e19094cd28e456541a8adf5439aa0cbca0ab9177
Budi Setiawan <budiskom99@gmail.com>
2025-05-24 20:54:18 +0700

    Update hardcode payment

--- constants/config.ts
+++ constants/config.ts
@@ -8,5 +8,9 @@ export const COMPANY_INFO = {
   name: 'Draclean Indonesia',
   address: 'Jl. Jeruk Purut, Cilandak Timur, Pasar Minggu, Jakarta Selatan, Indonesia',
   phone: '+6281386061072',
-  email: 'Dracelan@gmail.com'
+  email: 'Dracelan@gmail.com',
+  bankName: 'Bank BCA',
+    bankAccountName: 'Chandra Olopan Siallagan',
+    bankAccountNumber: '2860187542'
+    
 }
